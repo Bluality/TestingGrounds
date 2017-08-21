1f669de71859c4868c14f214e24673bfb8fcbbf8
LORIW10\Lori <jb10@hotmail.it>
2017-08-21 17:54:08 +0200

    Continua il trasferimento dell'IA in c++

--- Source/TestingGrounds/Public/ChooseNextWaypoint.h
+++ Source/TestingGrounds/Public/ChooseNextWaypoint.h
@@ -14,7 +14,14 @@ class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
 	
-		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
 	
 };
